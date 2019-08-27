f146cb959abf9415f1635789ce2d8e23af1fa0b9
Chaitenya Gupta <gupta@ucdavis.edu>
2019-08-27 23:51:35 +0400

    Initial Commit
    Added the first version of this tool.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Introduction
+Python-Image-Converter is a tool which helps convert images to any format. e.g. from jpg to png etc. Follow the instructions provided in usage to use this tool.
+
+# Inspiration
+It is really time consuming to access websites which help convert images from one format to another. This is where Python-Image-Converter steps in. 
+Without internet you can now convert your images into any format you like from the command line. 
+
+
+# Usage
+- First, you need to open up terminal and type the command ./setup.sh (If there is a message about permissions, you would the need to type the command chmod 777 setup.sh)
+- Now, you can simply type python3 image_converter.py jpg png -delete yes/no.
+- Hit enter and all jpg will now be converter to png and the jpg's might be deleted depending on the argument yes or no.
+- This works for all formats and you can now convert your images into pdf as well.
+
+# Contributions
+Chaitenya Gupta
+
+# License
+MIT**
---
+++ image_converter.py
@@ -0,0 +1,61 @@
+from PIL import Image
+import argparse
+import glob
+import os
+
+# The following function converts all file in original format
+# into new format.
+def fileConverter(originalFormat, newFormat):
+    for file in glob.glob("*." + str(originalFormat)):
+        image = Image.open(file)
+        rgbImage = image.convert('RGB')
+        rgbImage.save(file.replace(str(originalFormat), str(newFormat)), quality=100)
+
+# The following function calls fileConverter and also deletes the original
+# file if needed
+def start(originalFormat, newFormat, ans = 'no'):
+    print('Welcome to Python-Image-Converter!')
+    fileConverter(originalFormat, newFormat)
+    print('Your file is converted from ' + originalFormat + ' to ' + newFormat)
+
+    if ans in ['Yes', 'yes', '  Y', 'y']:
+        fileDeletion(originalFormat)
+        print('Deleted!')
+        print('Thank you for using Python-Image-Coverter')
+
+# Function called if original file needs to be deleted
+def fileDeletion(originalFormat):
+    for file in glob.glob("*." + originalFormat):
+        os.remove(file)
+
+# Function which acts as a CLI for this tool
+def main():
+    parser = argparse.ArgumentParser(prog='Python-Image-Converter',
+                                     usage='''
+                                    Usage:
+                                    Enter the command in ther following format:
+                                    python3 image_converter.py [Original extension. (e.g. png)] [New extension. (e.g. jpg)] [Yes/No]
+
+                                    e.g. python3 image_converter.py png jpg --del yes
+                                    ''',
+                                     description='''
+                                    -----------------------------------------------
+                                    Description:
+                                    This tool will convert your image files
+                                    -----------------------------------------------
+                                    ''',
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     add_help=True
+                                     )
+
+    parser.add_argument("original", type=str, help="Enter original extension. (e.g. png)", metavar="Original Extension")
+    parser.add_argument("new", type=str, help="Enter new extension. (e.g. jpg)", metavar="New Extension")
+    parser.add_argument("-delete", "-d", type=str,
+                        help="Optional: Enter yes to delete original file OR no to keep original file", default='no',
+                        required=False)
+
+    arg = parser.parse_args()
+    start(arg.original, arg.new, arg.delete)
+
+if __name__ == '__main__':
+    main()
